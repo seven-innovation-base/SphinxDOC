be3f5fc632dcca543bb744aebaf846d4d3c9eb88
yeshan333 <1329441308@qq.com>
2020-01-20 17:16:39 +0800

    chore: add netlify.toml

--- conf.py
+++ conf.py
@@ -46,7 +46,8 @@ language = 'zh_CH'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv', '.gitignore', 'LICENSE', 'README.md', 'requirements.txt']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv', \
+    '.gitignore', 'LICENSE', 'README.md', 'requirements.txt', 'netlify.toml']
 
 # https://www.sphinx.org.cn/usage/configuration.html#confval-exclude_patterns
 # html_extra_path = ['venv', '.gitignore']
